38dfb144013e004fd832b2dcc443a21d76f9c052
Faturrahman Ma'ruf <pedro@MacBook-Air-M1.local>
2022-06-11 00:14:23 +0700

    create simple web using FastAPI

---
+++ app/main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/hello")
+def hello():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
